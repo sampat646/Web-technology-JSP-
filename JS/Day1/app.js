ba429d0a0d5a7f5dc3dd9458c1f01391511f8240
sampat karehonna <sampatkarehonna@gmail.com>
2025-03-12 10:22:25 +0530

    Day one js eg problems added

--- JS/Day1/app.js
+++ JS/Day1/app.js
@@ -7,27 +7,35 @@
 
 
 //once we declare the let we can't again declare the but we re init
-let a;
-a = 10
+// let a;
+// a = 10
 // let a =10 // we can't re declare this 
-console.log(a);
+// console.log(a);
 
 //const we only declaration time only we have to init
-const b=19
+// const b=19
 //write a program to check weather person is eligible for voting or not
 // let age = prompt("Enter the age")
 // console.log(age>=18?"Yes eligible":"Not eligible");
 
-let kannada = prompt("Enter the Kannada marks")
-let Hindi = prompt("Enter the Hindi marks")
-let English = prompt("Enter the English marks")
-console.log(kannada,Hindi,English);
+// let kannada = prompt("Enter the Kannada marks")
+// let Hindi = prompt("Enter the Hindi marks")
+// let English = prompt("Enter the English marks")
+// console.log(kannada,Hindi,English);
 
-if (kannada <35 ||Hindi <35 ||English < 35) {
-    console.log("You failed");
+// if (kannada <35 ||Hindi <35 ||English < 35) {
+//     console.log("You failed");
     
-}else{
-    console.log("You passed enjoy");
+// }else{
+//     console.log("You passed enjoy");
     
+// }
+let sum=1
+let i=1
+while(i<10){
+    sum*=i
+    i++;
 }
+console.log(sum);
+
 
