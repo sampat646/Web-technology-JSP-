22aba9299db44f10c8354231b48054dcafda2055
sampat karehonna <sampatkarehonna@gmail.com>
2025-03-12 10:06:27 +0530

    Day one js eg problems

--- JS/Day1/app.js
+++ JS/Day1/app.js
@@ -12,7 +12,22 @@ a = 10
 // let a =10 // we can't re declare this 
 console.log(a);
 
+//const we only declaration time only we have to init
+const b=19
+//write a program to check weather person is eligible for voting or not
+// let age = prompt("Enter the age")
+// console.log(age>=18?"Yes eligible":"Not eligible");
 
+let kannada = prompt("Enter the Kannada marks")
+let Hindi = prompt("Enter the Hindi marks")
+let English = prompt("Enter the English marks")
+console.log(kannada,Hindi,English);
 
-
+if (kannada <35 ||Hindi <35 ||English < 35) {
+    console.log("You failed");
+    
+}else{
+    console.log("You passed enjoy");
+    
+}
 
