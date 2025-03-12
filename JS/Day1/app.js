fb1d203ffa4d67638815044bec9368876169bfd4
sampat karehonna <sampatkarehonna@gmail.com>
2025-03-12 09:23:09 +0530

    Day one js

---
+++ JS/Day1/app.js
@@ -0,0 +1,18 @@
+//Once we declare the var we can re init also and re declare also
+//var a;
+// var a = 10
+// var a = 20
+// a=34
+// console.log(a);
+
+
+//once we declare the let we can't again declare the but we re init
+let a;
+a = 10
+// let a =10 // we can't re declare this 
+console.log(a);
+
+
+
+
+
