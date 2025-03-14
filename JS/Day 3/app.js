701e4eb2e32e23d84b139d7d565537dd1f9c4d77
sampat karehonna <sampatkarehonna@gmail.com>
2025-03-14 10:28:08 +0530

    Day 3 hoisting

--- JS/Day 3/app.js
+++ JS/Day 3/app.js
@@ -23,7 +23,7 @@ console.log(greet("sam"));
 /*
 call a function: a function calling as parameter to another function
 */
-let sum = (a,b)=>{
+let sum = (a,b,c)=>{
     return a+b;
 }
 let sub = (a,b)=>{
@@ -46,5 +46,72 @@ function calculate(a,b,sum,sub,mul,div){
     
 }
 
-console.log(calculate(10,20,sum,sub,mul,div));
+// console.log(calculate(10,20,sum,sub,mul,div));
+
+// TODO Hoisting Variable hoisting (var)
+// console.log(x);//undefined
+// var x= 3;
+// console.log(x);//3
+
+// var x;
+// console.log(x);//undefined
+// x= 3
+// console.log(x);//3
+
+// TODO Function Declaration hoisting
+
+// sayWish()//Happy holi
+// function sayWish() {
+//     console.log("Happy holi");
+    
+// }
+
+
+//TODO function expression vs Declaration
+// sayHi()//TypeError: sayHi is not a function
+// var sayHi = function(){
+//     console.log("Hi!!!!!!!!");
+    
+// }
+
+// var sayHi;
+// sayHi()TypeError: sayHi is not a function
+// sayHi = ()=>{
+//     console.log("Hii");
+    
+// }
+
+// function processUser(userId) {
+//     if (!userId) {
+//         var result = "No user provided"; // Hoisted to top of function
+//     }
+//     console.log(result); // Output: "No user provided" or undefined
+//     var result = "User ID: " + userId; // Overwrites result
+//     return result;
+// }
+
+// console.log(processUser(null)); // Output: "User ID: null"
+
+
+// {
+//     console.log(x);
+//     var x =1;
+    
+// }
+// console.log(x);//1
+
+// {
+//     console.log(y);
+//     let y = 3
+    
+// }
+
+
+// console.log(y);//ReferenceError: Cannot access 'y' before initialization
+
+
+
+
+
+
 
